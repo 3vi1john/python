972c3dc164ea70a9fb76d1b9ab097dfa37866440
3vi1john <jbabio@me.com>
2013-05-29 10:30:46 -0400

    First Commit

---
+++ ZBFW.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+
+interfaces = input("Please enter number of interfaces?\n")
+#Basic Zone Based Firewall config
+inside = raw_input("Please specify the name of the inside lan interface? Ex. FastEthernet0/1\n")
+outside = raw_input("Please spcify the name of the outside interface? Ex. FastEthernet0/0\n")
+print ("\n")
+print ("!!Copy after this line down!!\n")
+print ("policy-map type inspect InsideToOutside")
+print ("class class-default")
+print ("inspect")
+print ("!")
+print ("zone security Inside")
+print ("description Inside network")
+print ("zone security Outside")
+print ("description Outside network")
+print ("zone-pair security InsideToOutside source Inside destination Outside")
+print ("service-policy type inspect InsideToOutside")
+print ("!")
+print ("interface %s" %inside)
+print ("zone-member security Inside")
+print ("!")
+print ("interface %s" %outside)
+print ("zone-member security Outside\n")
+print ("!!End of config!!")
